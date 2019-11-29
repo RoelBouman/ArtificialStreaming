2e6d99382bbc01cf269ccaf3ad08d4b8c379c83d
Roel Bouman <roelbouman@gmail.com>
2019-11-29 19:17:24 +0100

    Dual streaming with working left outer join

--- analyze_dual_csvstream.py
+++ analyze_dual_csvstream.py
@@ -4,11 +4,10 @@ from pyspark.sql import SparkSession
 from pyspark.sql.types import DoubleType
 from pyspark.sql.types import StructField 
 from pyspark.sql.types import StructType
-from pyspark.sql.types import StringType
 from pyspark.sql.types import TimestampType
-from pyspark.sql.functions import unix_timestamp
 from pyspark.sql.functions import col
 from pyspark.sql.functions import max as max_
+from pyspark.sql.functions import expr
                                           
                                                                                 
 if __name__ == "__main__":                                                      
@@ -24,7 +23,8 @@ if __name__ == "__main__":
   # The last entry is a timestamp
 
   beamformedFieldTypes = [StructField("V"+str(i), DoubleType(), False) for i in range(0,960)]
-  beamformedFieldTypes.append(StructField("timestamp", TimestampType(), False))
+  beamformedFieldTypes.append(StructField("beamformedtimestamp", TimestampType(), False))
+  beamformedFieldTypes.append(StructField("hourly_beamformedtimestamp", TimestampType(), False))
   beamformedSchema = StructType(beamformedFieldTypes) 
   beamformedDF = spark \
     .readStream \
@@ -34,8 +34,8 @@ if __name__ == "__main__":
     .csv("/opt/spark-data/beamformed") 
  
   weatherFieldTypes = [StructField("V"+str(i), DoubleType(), True) for i in range(0,22)]
-  weatherFieldTypes.append(StructField("timestamp", TimestampType(), False))
-  weatherSchema = StructTypeweatherFieldTypes) 
+  weatherFieldTypes.append(StructField("weathertimestamp", TimestampType(), False))
+  weatherSchema = StructType(weatherFieldTypes) 
   weatherDF = spark \
     .readStream \
     .option("sep", ",") \
@@ -43,13 +43,23 @@ if __name__ == "__main__":
     .schema(weatherSchema) \
     .csv("/opt/spark-data/weather")
 
-  # Joining dataframes
-  #...
+  # Watermark data
+  watermarkedBeamformedDF = beamformedDF.withWatermark("beamformedtimestamp", "1 hours")
+  watermarkedWeatherDF = weatherDF.withWatermark("weathertimestamp", "2 hours")
 
+  # Joining dataframes
+  joinedDF = watermarkedBeamformedDF.join(
+    watermarkedWeatherDF,
+    expr("""
+      hourly_beamformedtimestamp = weathertimestamp AND
+      beamformedtimestamp >= weathertimestamp AND
+      beamformedtimestamp <= weathertimestamp + interval 1 hour
+      """),
+    "leftOuter")
 
   # Random operation on joined data
   #...
-                   
+  maxTimeStamp = joinedDF.groupby("beamformedtimestamp", "weathertimestamp").agg(max_("beamformedtimestamp"))                    
 
   # Start running the query that prints the running counts to the console     
   query = maxTimeStamp \
